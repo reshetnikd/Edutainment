9b4b18c04573400fd810a9bf88e7f1919034a5be
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-02 22:20:35 +0200

    Redesign UI and add dynamic view with questions and TextField for answers

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -13,32 +13,70 @@ struct ContentView: View {
     @State private var numberOfTables = 1
     @State private var numberOfQuestions = 1
     @State private var questions = [5, 10, 15, 144]
-    @State private var multiplicationTables = []
+    @State private var multiplicationTables = [String]()
+    @State private var isShowingQuestions = false
+    @State private var answers = [String]()
     
     var body: some View {
-        Form {
-            Section(header: Text("Witch multiplication table you want to practice?")) {
-                Stepper(value: $numberOfTables, in: 1...12) {
-                    Text("Up to \(numberOfTables)")
-                }
-                
-                Picker("How many questions", selection: $numberOfQuestions) {
-                    ForEach(0..<questions.count) {
-                        if $0 == self.questions.count - 1 {
-                            Text("All")
-                        } else {
-                            Text("\(self.questions[$0])")
+        NavigationView {
+            VStack {
+                Form {
+                    if !isShowingQuestions {
+                        Section(header: Text("Witch multiplication tables?")) {
+                            Stepper(value: $numberOfTables, in: 1...12) {
+                                Text("Up to \(numberOfTables)")
+                            }
+                        }
+                        
+                        Section(header: Text("How many questions?")) {
+                            Picker("How many questions", selection: $numberOfQuestions) {
+                                if numberOfTables > 1 {
+                                    ForEach(0..<questions.count) {
+                                        if $0 == self.questions.count - 1 {
+                                            Text("All")
+                                        } else {
+                                            Text("\(self.questions[$0])")
+                                        }
+                                    }
+                                } else {
+                                    ForEach(0..<2) {
+                                        Text("\(self.questions[$0])")
+                                    }
+                                }
+                            }
+                            .pickerStyle(SegmentedPickerStyle())
+                        }
+                    } else {
+                        Section(header: Text("Answer the questions.")) {
+                            ForEach(0..<self.questions[self.numberOfQuestions]) {
+                                TextField("\(self.multiplicationTables[$0]) =", text: self.$answers[$0], onCommit: self.checkAnswer)
+                                    .keyboardType(.numberPad)
+                            }
                         }
                     }
-                }.pickerStyle(SegmentedPickerStyle())
+                }
                 
                 Button(action: {
                     self.generateTables(to: self.numberOfTables)
+                    self.questions[self.questions.count - 1] = self.numberOfTables * 12
+                    for _ in 0..<self.questions[self.numberOfQuestions] {
+                        self.answers.append("")
+                    }
+                    self.isShowingQuestions.toggle()
                 }) {
-                    Text("Play")
+                    if self.isShowingQuestions {
+                        Text("Check")
+                    } else {
+                        Text("Play")
+                    }
                 }
             }
+            .navigationBarTitle("Edutainment")
         }
+    }
+
+    func checkAnswer() {
+        // function body
     }
     
     func generateTables(to number: Int) {
@@ -57,9 +95,7 @@ struct ContentView: View {
             }
         }
         
-        for table in multiplicationTables {
-            print(table)
-        }
+        multiplicationTables.shuffle()
     }
 }
 
